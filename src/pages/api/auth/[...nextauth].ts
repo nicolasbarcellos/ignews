2aa5b8ef7ff3a7803b2ea55d28b0ec992fee72b8
Nicolas <nicolasbarcellos95@gmail.com>
2021-08-05 13:49:43 -0300

    first commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,19 +12,33 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
-  jwt: {
-    signingKey: process.env.SIGNING_KEY
-  },
   callbacks: {
+    // sempre que usuário faz login na aplicação
     async signIn(user, account, profile) {
       const { email } = user;
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection("users"), 
-            { data: { email } }
-          )
+         q.If(
+           q.Not(
+             q.Exists(
+               q.Match(
+                 q.Index('user_by_email'),
+                 q.Casefold(user.email)
+               )
+             )
+           ),
+           q.Create(
+             q.Collection('users'),
+             { data: { email } }
+           ),
+           q.Get(
+             q.Match(
+               q.Index('user_by_email'),
+               q.Casefold(user.email)
+             )
+           )
+         )
         )
 
         return true;
